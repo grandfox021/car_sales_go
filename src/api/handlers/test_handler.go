0b78cc06a90152040f1ddb6106c95ae1c15cc4ac
grandfox021 <hbtk320@gmail.com>
2026-06-05 21:09:31 +0330

    add different bindings for test

--- src/api/handlers/test_handler.go
+++ src/api/handlers/test_handler.go
@@ -6,6 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+
+
+type Person struct{
+
+	Firstname  string
+	Phone 	  int
+
+}
+
+type testHeaderBinder struct {
+	Userid		string
+	Browser		string
+}
+
 type TestHandler struct {
 
 }
@@ -45,5 +59,109 @@ func (h *TestHandler) Test_user_handler_post_by_username(c *gin.Context){
 		"called_username" : username,
 		"called_id" : id,
 		"success" : true,
+	})
+}
+
+func (h *TestHandler) Test_headerbineder1(c *gin.Context){
+
+	user_id := c.GetHeader("user_id")
+
+	c.JSON(http.StatusOK, gin.H{
+		"result" : "Test_headerbineder1 !",
+		"success" : true,
+		"user_id" : user_id,
+	})
+}
+
+func (h *TestHandler) Test_headerbineder2(c *gin.Context) {
+
+	header := testHeaderBinder{}
+
+	err := c.BindHeader(&header)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"result":  "Test_headerbineder2 !",
+			"success": false,
+			"error":   err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result":        "Test_headerbineder2 !",
+		"success":       true,
+		"binded_header": header,
+	})
+}
+
+func (h *TestHandler) Test_querybinder1(c *gin.Context) {
+
+	id := c.Query("id")
+	name := c.Query("name")
+	c.JSON(http.StatusOK, gin.H{
+		"result": "Test_querybinder1 !",
+		"success":true,
+		"id":id ,
+		"name":name ,
+
+	})
+}
+
+func (h *TestHandler) Test_querybinder2(c *gin.Context) {
+
+	ids := c.QueryArray("id")
+	name := c.Query("name")
+	c.JSON(http.StatusOK, gin.H{
+		"result": "Test_querybinder1 !",
+		"success":true,
+		"ids":ids ,
+		"name":name ,
+
+	})
+}
+
+func (h *TestHandler) Test_bodybinder(c *gin.Context) {
+
+	person := Person{}
+
+	c.ShouldBindJSON(&person)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "Test_bodybinder !",
+		"success":true,
+		"bodybinder": person,
+
+	})
+}
+
+func (h *TestHandler) Test_formbinder(c *gin.Context) {
+
+	person := Person{}
+
+	c.ShouldBind(&person)
+	c.JSON(http.StatusOK, gin.H{
+		"result": "Test_bodybinder !",
+		"success":true,
+		"formbinder": person,
+
+	})
+}
+
+
+func (h *TestHandler) Test_filebinder(c *gin.Context) {
+
+	file , _ := c.FormFile("file")
+	err := c.SaveUploadedFile(file,"file")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error" : err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "Test_filebinder !",
+		"success":true,
+		"file": file.Filename,
+
 	})
 }
--- src/api/routers/testrouter.go
+++ src/api/routers/testrouter.go
@@ -11,7 +11,16 @@ func Testrouter(r *gin.RouterGroup){
 	r.GET("/" , test_handler.TestHandler)
 	r.GET("/user/:id" , test_handler.Test_user_handler_by_id)
 	r.POST("/user/:username/post/:id", test_handler.Test_user_handler_post_by_username)
-	
+	r.POST("/user/headerbind1",test_handler.Test_headerbineder1)
+	r.POST("/user/headerbind2",test_handler.Test_headerbineder2)
 
+	r.POST("/user/querybinder1",test_handler.Test_querybinder1)
+	r.POST("/user/querybinder2",test_handler.Test_querybinder2)
+
+	r.POST("/user/bodybinder",test_handler.Test_bodybinder)
+
+	r.POST("/user/formbinder",test_handler.Test_formbinder)
+
+	r.POST("/user/filebinder",test_handler.Test_filebinder)
 
 }
